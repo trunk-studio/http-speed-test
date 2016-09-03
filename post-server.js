5ace1f4331927eb4957fc80385842aee3a47c98e
Kyle <kyle@trunk-studio.com>
2016-09-03 16:58:34 +0800

    update

--- post-server.js
+++ post-server.js
@@ -1,34 +1,28 @@
 http = require('http');
-fs = require('fs');
 server = http.createServer( function(req, res) {
-
-    console.dir(req.param);
-
     if (req.method == 'POST') {
-        console.log("POST");
+        //console.log("POST");
         var body = '';
         req.on('data', function (data) {
             body += data;
-            console.log("Partial body: " + body);
+            //console.log("Partial body: " + body);
         });
         req.on('end', function () {
-            console.log("Body: " + body);
+            //console.log("Body: " + body);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('size=' + Buffer.byteLength(body, 'utf8'));
         });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
     }
     else
     {
-        console.log("GET");
+        //console.log("GET");
         //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-        var html = fs.readFileSync('index.html');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('ok');
     }
-
 });
 
-port = 3000;
-host = '127.0.0.1';
+port = 5999;
+host = '0.0.0.0';
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
